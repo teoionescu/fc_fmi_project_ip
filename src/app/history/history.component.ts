1995f59cce3bdd85b243d522c55d31ccc3c032c1
Radu Muntean <radu.muntean.2@gmail.com>
2019-05-23 02:19:59 +0300

    added the History component

---
+++ src/app/history/history.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'History',
+  templateUrl: './history.component.html',
+  moduleId: module.id,
+})
+export class HistoryComponent implements OnInit {
+  constructor() {
+    // Use the constructor to inject services.
+  }
+
+  ngOnInit(): void {
+      // Use the "ngOnInit" handler to initialize data for the view.
+  }
+}
