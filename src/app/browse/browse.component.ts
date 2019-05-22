0eb12f264c5f5572ca1d0bd5d99997a5a4056762
teoionescu <teoionescu32@gmail.com>
2019-05-22 23:10:37 +0300

    nativescript cli

---
+++ src/app/browse/browse.component.ts
@@ -0,0 +1,67 @@
+import { Component, OnInit } from "@angular/core";
+
+import { takePicture, requestPermissions } from 'nativescript-camera';
+import { ImageAsset } from 'tns-core-modules/image-asset';
+
+@Component({
+    selector: "Browse",
+    moduleId: module.id,
+    templateUrl: "./browse.component.html"
+})
+export class BrowseComponent implements OnInit {
+    public saveToGallery: boolean = false;
+    public allowsEditing: boolean = false;
+    public keepAspectRatio: boolean = true;
+    public width: number = 320;
+    public height: number = 240;
+    public cameraImage: ImageAsset;
+    public actualWidth: number;
+    public actualHeight: number;
+    public scale: number = 1;
+    public labelText: string;
+
+    onTakePictureTap(args) {
+        requestPermissions().then(
+            () => {
+                takePicture({ width: this.width, height: this.height, keepAspectRatio: this.keepAspectRatio, saveToGallery: this.saveToGallery, allowsEditing: this.allowsEditing })
+                    .then((imageAsset: any) => {
+                        this.cameraImage = imageAsset;
+                        let that = this;
+                        imageAsset.getImageAsync(function (nativeImage, ex) {
+                            if (ex instanceof Error) {
+                                throw ex;
+                            } else if (typeof ex === "string") {
+                                throw new Error(ex);
+                            }
+
+                            if (imageAsset.android) {
+                                // get the current density of the screen (dpi) and divide it by the default one to get the scale
+                                that.scale = nativeImage.getDensity() / android.util.DisplayMetrics.DENSITY_DEFAULT;
+                                that.actualWidth = nativeImage.getWidth();
+                                that.actualHeight = nativeImage.getHeight();
+                            } else {
+                                that.scale = nativeImage.scale;
+                                that.actualWidth = nativeImage.size.width * that.scale;
+                                that.actualHeight = nativeImage.size.height * that.scale;
+                            }
+                            that.labelText = `Displayed Size: ${that.actualWidth}x${that.actualHeight} with scale ${that.scale}\n` +
+                                `Image Size: ${Math.round(that.actualWidth / that.scale)}x${Math.round(that.actualHeight / that.scale)}`;
+
+                            console.log(`${that.labelText}`);
+                        });
+                    }, (error) => {
+                        console.log("Error: " + error);
+                    });
+            },
+            () => alert('permissions rejected')
+        );
+    }
+    
+    constructor() {
+        // Use the component constructor to inject providers.
+    }
+
+    ngOnInit(): void {
+        // Use the "ngOnInit" handler to initialize data for the view.
+    }
+}
